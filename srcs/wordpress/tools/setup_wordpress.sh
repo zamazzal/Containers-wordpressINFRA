458318a468ce61b7d013f25c33ba03de78837236
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-18 01:28:51 +0000

    wordpress

---
+++ srcs/wordpress/tools/setup_wordpress.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
+
+mkdir -p /var/www/html/wordpress
+tar xzf /tmp/latest.tar.gz --strip-components=1 -C /var/www/html/wordpress/
+cp /var/www/html/wordpress/wp-config{-sample,}.php
+
+echo "<?php
+define( 'DB_NAME', '$MYSQL_DATABASE' );
+define( 'DB_USER', '$MYSQL_USER' );
+define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );
+define( 'DB_HOST', '$WORDPRESS_DB_HOST' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+\$table_prefix = '$WORDPRESS_TABLE_PREFIX';
+
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+" >  /var/www/html/wordpress/wp-config.php
+
+curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/html/wordpress/wp-config.php
+
+fi
+chown -R www-data:www-data  /var/www/html/wordpress
+
+/usr/sbin/php-fpm7.3 -FR
