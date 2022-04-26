7350cc2ca4c82545e87656e3604f84ad7155e488
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-26 22:26:32 +0000

    ftpFix

--- srcs/bonus/FTPServer/tools/setup_ftp.sh
+++ srcs/bonus/FTPServer/tools/setup_ftp.sh
@@ -2,12 +2,16 @@
 
 mkdir -p /var/run/vsftpd/empty
 touch /etc/vsftpd.chroot_list /etc/vsftpd/chroot_list
+echo $FTP_USER | tee -a /etc/vsftpd.userlist
+
 if id "$FTP_USER" &>/dev/null; then
     echo 'user found'
 else
     adduser $FTP_USER --disabled-password
     echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd > /dev/null
     chown -R $FTP_USER:$FTP_USER /home/wordpress
+    chmod a-w -R /home/wordpress
+    chmod -R 775 /home/wordpress
     usermod -d /home/wordpress/ $FTP_USER
 fi
 
