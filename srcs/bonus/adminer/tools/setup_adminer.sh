8f8a97a73e55999f28ffec963f7bb5f396b36b5e
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-27 19:08:17 +0000

    adminer

---
+++ srcs/bonus/adminer/tools/setup_adminer.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+if [ ! -f "/var/www/adminer/.ready" ]; then
+wget "http://www.adminer.org/latest.php" -O /var/www/adminer/index.php
+touch /var/www/adminer/.ready
+fi
+chown -R www-data:www-data /var/www/adminer/
+chmod 755 /var/www/adminer/
+
+/usr/sbin/php-fpm7.3 -FR
--- srcs/nginx/tools/setup_nginx.sh
+++ srcs/nginx/tools/setup_nginx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sed -i 's@{{DomainName}}@'"$DOMAIN_NAME"'@' /etc/nginx/sites-enabled/inception.php
-sed -i 's@{{CertifPATH}}@'"$CERTS"'@' /etc/nginx/sites-enabled/inception.php
-sed -i 's@{{KeyPATH}}@'"$CERTS_KEY"'@' /etc/nginx/sites-enabled/inception.php
+sed -i 's@{{DomainName}}@'"$DOMAIN_NAME"'@' /etc/nginx/sites-enabled/inception.conf
+sed -i 's@{{CertifPATH}}@'"$CERTS"'@' /etc/nginx/sites-enabled/inception.conf
+sed -i 's@{{KeyPATH}}@'"$CERTS_KEY"'@' /etc/nginx/sites-enabled/inception.conf
+
+sed -i 's@{{DomainName}}@'"$DOMAIN_NAME"'@' /etc/nginx/sites-enabled/adminer.conf
+sed -i 's@{{CertifPATH}}@'"$CERTS"'@' /etc/nginx/sites-enabled/adminer.conf
+sed -i 's@{{KeyPATH}}@'"$CERTS_KEY"'@' /etc/nginx/sites-enabled/adminer.conf
 # start nginx
 echo "127.0.0.1	$DOMAIN_NAME" >> /etc/hosts
+echo "127.0.0.1	adminer-$DOMAIN_NAME" >> /etc/hosts
+echo "127.0.0.1	static-$DOMAIN_NAME" >> /etc/hosts
+
 nginx -g 'daemon off;'
