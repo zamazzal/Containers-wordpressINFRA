6c27bdd0ce6c098536ef5c68e4fab0368d8d420a
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-28 01:54:42 +0000

    gitwatch

---
+++ srcs/bonus/gitwatch/tools/setup_gitwatch.sh
@@ -0,0 +1,14 @@
+cd /var/www/wordpress
+if [ ! -d "/var/www/wordpress/.git" ]; then
+git init /var/www/wordpress
+git config --global user.email $GIT_AUTHOR
+git fetch $GIT_REMOTE
+echo "inception" > README.md
+git add .
+git commit -m "gitwatch sync"
+git branch -M $GIT_BRANCH
+git remote add origin $GIT_BRANCH
+git push $GIT_REMOTE $GIT_BRANCH
+fi
+
+gitwatch -r $GIT_REMOTE -b $GIT_BRANCH /var/www/wordpress/
