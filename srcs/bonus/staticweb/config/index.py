8cd0970246578d701072e344b87d344f5b8f1ca9
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-28 16:02:59 +0000

    newstatic

--- srcs/bonus/staticweb/config/index.py
+++ srcs/bonus/staticweb/config/index.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def index():
+    return send_from_directory("static", "index.html")
+
+@app.route("/<path:path>")
+def static_dir(path):
+    return send_from_directory("static", path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
