253e259e39ab18cb93af32cdabb4d38a0bbd7852
Zouhair AMAZZAL <Zouhair.AMAZZAL@4d.com>
2022-04-29 00:59:49 +0000

    addrootpasswd

--- srcs/mariadb/tools/setup_db.sh
+++ srcs/mariadb/tools/setup_db.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+mkdir -p /var/run/mysqld/
+chown -R mysql:mysql /var/run/mysqld/
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+chown -R mysql:mysql /var/lib/mysql
+/usr/bin/mysql_install_db --user=mysql --datadir='/var/lib/mysql' > /dev/null
 
-/usr/bin/mysql_install_db -user=mysql --datadir='/var/lib/mysql' > /dev/null
-
-/etc/init.d/mysql start
-
+service mysql start
+sleep 1
 expect -c "
 set timeout 10
 spawn /usr/bin/mysql_secure_installation
@@ -24,22 +25,22 @@ send \"y\r\"
 expect eof
 " > /dev/null
 
-echo "mysql << EOF
+echo "mysql -h localhost -u root << EOF
 USE mysql;
 create database $MYSQL_DATABASE;
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE . * TO '$MYSQL_USER'@'%';
+SET PASSWORD FOR root@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 FLUSH PRIVILEGES;
 EOF" > /tmp/database_setup.sh
 chmod 700 /tmp/database_setup.sh
 chmod +x /tmp/database_setup.sh
 /tmp/database_setup.sh  2>&1 > /tmp/loglol
 rm -f /tmp/database_setup
-
 fi
 
-/etc/init.d/mysql stop
+mysqladmin shutdown -u root -p$MYSQL_ROOT_PASSWORD
 
 # You can start the MariaDB daemon with:
 echo "starting mysql..."
-exec /usr/bin/mysqld_safe --user=mysql --datadir='/var/lib/mysql'
+exec /usr/bin/mysqld_safe --user=mysql --skip-grant-tables --datadir='/var/lib/mysql'
